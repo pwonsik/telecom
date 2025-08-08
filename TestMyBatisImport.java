1c1e7cf8e012b355d131dbeffd6781c506c8fd4c
강인수 <boykis82@gmail.com>
2025-08-08 17:41:26 +0900

    불필요파일삭제2

--- TestMyBatisImport.java
+++
@@ -1,7 +0,0 @@
-// 임시 테스트 파일 - MyBatis import 확인용
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-public class TestMyBatisImport {
-    // 컴파일 테스트만을 위한 임시 파일
-}
